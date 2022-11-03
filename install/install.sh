d131613d71b9d8f319b5c3f751324a51ad1aa965
ChingKai Tseng <83930467+tseng91301@users.noreply.github.com>
2022-11-03 20:06:53 +0800

    a3

--- install/install.sh
+++
@@ -1,6 +0,0 @@
-pwd
-#find /home |grep 'send.cm無限儲存空間計畫/install/install.sh'
-installpath=$("pwd")|$("grep 'w'")
-
-#installpath=$("find /home |grep 'send.cm無限儲存空間計畫/install/install.sh'")
-echo "fffffff: $installpath"
