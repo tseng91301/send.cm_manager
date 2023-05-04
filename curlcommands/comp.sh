1c0870ac0a42be8f473061a44a78a3d6432c849a
ChingKai Tseng <83930467+tseng91301@users.noreply.github.com>
2023-05-04 18:37:22 +0800

    2023-05-04

--- curlcommands/comp.sh
+++
@@ -1,6 +0,0 @@
-g++ sendcmtool.cpp -o sendcmtool
-g++ uploadsendcm.cpp -o uploadsendcm
-g++ downloadsendcm.cpp -o downloadsendcm
-sudo cp sendcmtool /bin
-sudo cp uploadsendcm /bin
-sudo cp downloadsendcm /bin
